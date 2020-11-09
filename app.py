1581cc4edbff2cc9b1ba30e092e353a933d8a7a6
Prakhar Awasthi <64955939+prakharawasthi-ctrl@users.noreply.github.com>
2020-11-09 09:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
 db = SQLAlchemy(app)
 # 'mysql://root:@localhost/codin'
 
@@ -20,17 +20,17 @@ class Contacts(db.Model):
     email = db.Column(db.String(20), nullable=False)
 
 
-@app.route("https://hello-mom.herokuapp.com")
+@app.route("/")
 def hello():
     return render_template('index.html')
 
 
-@app.route("https://hello-mom.herokuapp.com/about")
+@app.route("/about")
 def about():
     return render_template('about.html')
 
 
-@app.route("https://hello-mom.herokuapp.com/post")
+@app.route("/post")
 def post():
     return render_template('post.html')
 
